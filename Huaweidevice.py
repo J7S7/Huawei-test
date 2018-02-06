059dcb1dd68e379a47d193ab2e6bc1d682b8c149
jinsi <413601082@qq.com>
2018-02-06 17:03:43 +0800

    2018/2/5test

--- Huaweidevice.py
+++ Huaweidevice.py
@@ -2,3 +2,25 @@
 import telnetlib
 import re
 
+class HuaweiSwitch(object):
+    def __init__(self,host=None,hostname=None,username=None,password=None,connected=False,port_lists=None):
+        self.host=host
+        self.hostname=hostname
+        self.username=username
+        self.password=password
+        self.connected=connected
+        self.port_lists=port_lists
+
+
+    def connect(self,host=None,port=23,timeout=2):
+        if host is None:
+            host = self.host
+
+        self._connection=telnetlib.telnet(host,port,timeout)
+        self._authenticate()
+        self._get_hostname()
+
+
+
+    def authenticate(self,username,password):
+        if 
