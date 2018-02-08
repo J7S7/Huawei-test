69b3c9b0e5f19d7f502b0a8df3b5475f1ec10797
jinsi <413601082@qq.com>
2018-02-08 17:03:18 +0800

    2018/2/8test

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from HuaweiSwitch.Huaweidevice import *
+from HuaweiSwitch.HuaweiSwitch import *
+
 
 test = HuaweiSwitch()
 test.host = '10.242.207.11'
 test.username = 'localadmin'
 test.password = 'wlkjhj2018'
 test.connect()
+test.test()
